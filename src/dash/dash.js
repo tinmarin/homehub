36b09c14d4213c667971794efe8b5582eaca5290
Sergio Marron <sergiolmarron@gmail.com>
2018-04-18 08:03:04 -0400

    minor changes

--- src/dash/dash.js
+++ src/dash/dash.js
@@ -6,7 +6,6 @@ const dudeButton = '88:71:e5:d6:32:ea'
 
 var Dash = function() {
 
-    console.log('On dash constructor................');
 
 };
 
@@ -14,8 +13,6 @@ var Dash = function() {
 
 Dash.prototype.init = function() {
 
-    console.log('Initializing listeners................');
-	
 	var dash = dash_button(dudeButton, null, null, 'all'); //address from step above
 
 	return dash;    
